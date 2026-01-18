9261142ec4dcb88e6d3790146465afc53cb7cd57
Jason Deng <jasondeng107@gmail.com>
2026-01-17 22:18:50 -0500

    testing 123

--- Frontend/src/components/TextPane.tsx
+++ Frontend/src/components/TextPane.tsx
@@ -2,7 +2,7 @@ export default function TextPane() {
   const features = [
     {
       title: "Real & Authentic Reviews",
-      description: "Read verified customer experiences, we NEVER allow fake or paid reviews!",
+      description: "Read verified customer experiences, we NEVER allow fake or paid reviews!", // diddy
       iconColor: "from-emerald-400 to-emerald-600",
       iconPath: "M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
     },
